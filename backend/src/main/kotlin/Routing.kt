0d9c3a36f361c4bd7c432d2c3afb54e991165047
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2025-02-01 17:23:02 +0000

    no tested but compiles

--- backend/src/main/kotlin/Routing.kt
+++ backend/src/main/kotlin/Routing.kt
@@ -1,16 +1,29 @@
 package com.example
 
-import io.ktor.serialization.kotlinx.json.*
+import com.example.transformer.Transform
+import com.example.transformer.Transformation
 import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import java.util.logging.Logger
 
-fun Application.configureRouting() {
+@Serializable
+data class PostRequest(
+    val html: String,
+)
+
+fun Application.configureRouting(logger: Logger) {
+    val transform = Transform(logger)
+
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        post {
+            val html = call.receive<PostRequest>().html
+
+            val transformations: List<Transformation> = transform.transform(html)
+
+            call.respond(transformations)
         }
     }
 }
