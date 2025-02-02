8863d5eee83c592356d7f3e3eb97957b7a571658
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2025-02-02 03:21:14 +0000

    formatting and general housekeeping

--- backend/src/main/kotlin/transformer/Transformer.kt
+++ backend/src/main/kotlin/transformer/Transformer.kt
@@ -1,8 +1,5 @@
 package com.example.transformer
 
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
