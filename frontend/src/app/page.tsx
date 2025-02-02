dd8fcac66c554bedd9ac862a47097781ad229f7a
Max Smith <103558620+Max-Sepp@users.noreply.github.com>
2025-02-02 06:58:00 +0000

    updated packages and made homepage form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -13,13 +13,17 @@ import {Label} from '@/components/ui/label';
 import {useRouter} from 'next/navigation';
 import {useState} from 'react';
 
+import type {FormEvent} from 'react';
+
 
 export default function HomePage() {
     const router = useRouter();
 
     const [url, setUrl] = useState('');
 
-    function redirectPage() {
+    function redirectPage(e: FormEvent) {
+        e.preventDefault();
+
         if (url != '') {
             router.push(`/url?url=${url}`);
         }
@@ -31,17 +35,20 @@ export default function HomePage() {
             <PageHeaderDescription>
                 Our tool takes an inaccessible website and transforms it by automatically generating meaningful alt text for images and adding labels to form elements. This makes the website more user-friendly and accessible, especially for individuals who rely on screen readers.
             </PageHeaderDescription>
-            <PageActions className="flex flex-col gap-4">
-                <div className="w-full">
-                    <Label htmlFor="url">URL:</Label>
-                    <Input
-                        type="url"
-                        id="url"
-                        placeholder="docsoc.co.uk"
-                        onChange={(event) => setUrl(event.target.value)}
-                    />
-                </div>
-                <Button onClick={redirectPage}>Submit</Button>
+            <PageActions>
+                <form className="w-full flex flex-col gap-4 items-center" onSubmit={redirectPage}>
+                    <div className="w-full">
+                        <Label htmlFor="url">URL:</Label>
+                        <Input
+                            type="url"
+                            id="url"
+                            placeholder="https://docsoc.co.uk"
+                            value={url}
+                            onChange={(event) => setUrl(event.target.value)}
+                        />
+                    </div>
+                    <Button className="max-w-72" type="submit">Submit</Button>
+                </form>
             </PageActions>
         </PageHeader>
     );
